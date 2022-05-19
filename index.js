d72be8f7b913f8f823e79675579d06bb4be01bad
Ashton Tobar <+>
2022-05-19 13:30:52 -0500

    commit tree 9bbf55684a1503ceff06f56eee745792a7edc29c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase());
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'susan';
+}
